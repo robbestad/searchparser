5468ccb15290af21883978e691c6ce9921a88365
Sven Anders Robbestad <robbestad@gmail.com>
2017-09-13 11:41:39 +0200

    Fix serialization problem

--- src/main/java/no/nrk/origo/searchparser/SampleController.java
+++ src/main/java/no/nrk/origo/searchparser/SampleController.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.*;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonWriter;
 import org.json.*;
 import com.google.gson.Gson;
 import org.restexpress.Request;
 import org.restexpress.Response;
 
-import java.lang.reflect.Type;
-import java.util.Collection;
 import java.util.List;
 
 public class SampleController {
@@ -20,55 +21,47 @@ public class SampleController {
         super();
     }
 
+//    static List getKeysFromJson(JsonElement jsonObject) throws Exception {
+//        Object things = new Gson().fromJson(jsonObject, Object.class);
+//        List keys = new ArrayList();
+//        collectAllTheKeys(keys, things);
+//        return keys;
+//    }
+//
+//    private static void collectAllTheKeys(List keys, Object o) {
+//        Collection values = null;
+//        if (o instanceof Map) {
+//            Map map = (Map) o;
+//            keys.addAll(map.keySet()); // collect keys at current level in hierarchy
+//            values = map.values();
+//        } else if (o instanceof Collection)
+//            values = (Collection) o;
+//        else // nothing further to collect keys from
+//            return;
+//
+//        for (Object value : values)
+//            collectAllTheKeys(keys, value);
+//    }
+
     public Object create(Request request, Response response) {
         //TODO: Your 'POST' logic here...
         return null;
     }
 
-    public Object read(Request request, Response response) throws IOException {
+    public Object read(Request request, Response response) throws Exception {
         response.setContentType("application/json");
         response.addHeader("Cache-Control", "no-cache");
-//        String json = "[{album_id:1,album_title:'album1'},{album_id:2,album_title:'album2'}]";
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("abc", "123456");
-        map.put("def", "hmm");
+        map.put("Key1", "123456");
+        map.put("Key2", "789123");
         list.add(map);
 
-        //1. Convert object to JSON string
-        Gson gson = new Gson();
-        String jsonFormattedString;
+        Gson gson = new GsonBuilder().setPrettyPrinting()
+                .create();
         String json = gson.toJson(list);
-        try {
-            jsonFormattedString = new JSONTokener(json).nextValue().toString();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        //2. Convert object to JSON string and save into a file directly
-        try (FileWriter writer = new FileWriter("list.json")) {
-
-            gson.toJson(list, writer);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        OutputStreamWriter out = new OutputStreamWriter(System.out);
-        JsonWriter writer = new JsonWriter(out);
-        //set indentation for pretty print
-        writer.setIndent("\t");
-        //start writing
-        writer.beginObject();
-
-        writer.name("myStuff").value(json);
-        writer.endObject();
-
-        writer.flush();
-        //close writer
-        writer.close();
-
-//        return  gson.toJson(list);
-        return json.replaceAll("\\\\", "");
+        response.noSerialization();
+        return json;
     }
 
     public List<Object> readAll(Request request, Response response) {
